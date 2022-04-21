6dc1e056a26b897dc33f55dfd6a3ff73218f9304
Aiviatrist <92304985+Aiviatrist@users.noreply.github.com>
2022-04-21 19:31:39 +1000

    Add files via upload

--- ajavasf.js
+++ ajavasf.js
@@ -1,5 +1,9 @@
 song1="";
 song2="";
+leftx=0;
+rightx=0;
+lefty=0;
+righty=0;
 function preload(){
     song1=loadSound("music.mp3");
     song2=loadSound("music2.mp3");
@@ -10,6 +14,20 @@ function setup(){
 
     video= createCapture(VIDEO);
     video.hide();
+    posenet=ml5.poseNet(video,modelloaded)
+    posenet.on("pose",gotPoses);
+}
+function modelloaded(){
+    console.log("Model Loaded");
+}
+function gotPoses(results){
+     if(results.length>0){
+        console.log(results);
+        leftx=results[0].pose.leftWrist.x;
+        lefty=results[0].pose.leftWrist.y;
+        rightx=results[0].pose.rightWrist.x;
+        righty=results[0].pose.rightWrist.y;
+     }
 }
 function draw(){
     image(video,0,0,600,500);
