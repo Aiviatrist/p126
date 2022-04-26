60825b1197af15affb94ba05134300a7233bcea6
Aiviatrist <92304985+Aiviatrist@users.noreply.github.com>
2022-04-26 19:36:59 +1000

    Add files via upload

--- ajavasf.js
+++ ajavasf.js
@@ -4,6 +4,9 @@ leftx=0;
 rightx=0;
 lefty=0;
 righty=0;
+slw=0;
+s1="";
+s2="";
 function preload(){
     song1=loadSound("music.mp3");
     song2=loadSound("music2.mp3");
@@ -27,8 +30,18 @@ function gotPoses(results){
         lefty=results[0].pose.leftWrist.y;
         rightx=results[0].pose.rightWrist.x;
         righty=results[0].pose.rightWrist.y;
+        slw=results[0].pose.keypoints[9].score;
      }
 }
 function draw(){
     image(video,0,0,600,500);
+    s1=song1.isPlaying();
+    if(slw>0.2){
+        circle(leftx,lefty,20);
+        song1.stop();
+        if(s1==false){
+            song.play();
+            document.getElementById("song").innerHTML="Song name-Peter Pan";
+        }
+    }
 }
