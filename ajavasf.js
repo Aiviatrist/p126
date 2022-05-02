ed212fa87b37450913d2dfc7ea7ae7a64b1d32c1
Aiviatrist <92304985+Aiviatrist@users.noreply.github.com>
2022-05-02 19:02:31 +1000

    Add files via upload

--- ajavasf.js
+++ ajavasf.js
@@ -7,6 +7,7 @@ righty=0;
 slw=0;
 s1="";
 s2="";
+srw=0;
 function preload(){
     song1=loadSound("music.mp3");
     song2=loadSound("music2.mp3");
@@ -31,17 +32,27 @@ function gotPoses(results){
         rightx=results[0].pose.rightWrist.x;
         righty=results[0].pose.rightWrist.y;
         slw=results[0].pose.keypoints[9].score;
+        srw=results[0].pose.keypoints[10].scrore;
      }
 }
 function draw(){
     image(video,0,0,600,500);
     s1=song1.isPlaying();
+    s2=song2.isPlaying();
     if(slw>0.2){
         circle(leftx,lefty,20);
         song1.stop();
-        if(s1==false){
-            song.play();
+        if(s2==false){
+            song2.play();
             document.getElementById("song").innerHTML="Song name-Peter Pan";
+        }
+    }
+    if(srw>0.2){
+        circle(rightx,righty,20);
+        song2.stop();
+        if(s1==false){
+            song1.play();
+            document.getElementById("song").innerHTML="Song name-Harry Potter";
         }
     }
 }
